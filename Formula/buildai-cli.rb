aec010d4cc5d0ee9c7fab3d42691f974c939ee64
MadR7 <95593863+MadR7@users.noreply.github.com>
2026-03-14 04:28:57 +0000

    chore: update buildai-cli to 0.2.0

--- Formula/buildai-cli.rb
+++ Formula/buildai-cli.rb
@@ -3,9 +3,9 @@ class BuildaiCli < Formula
   preserve_rpath
   desc "Build AI CLI"
   homepage "https://github.com/build-ai-ego/data-build-ai"
-  url "https://files.pythonhosted.org/packages/fc/17/5922b524f93b43d2dd7fbc9c8f99d2526327a1b5a4ca13fad10ddd30628b/buildai_cli-0.1.0-py3-none-any.whl"
-  sha256 "7d79c97da1af1256d4565af020d818ed985740e3e1fc0f97103df8b55ff7d18e"
-  version "0.1.0"
+  url "https://files.pythonhosted.org/packages/55/e4/16c25ce69b2fde0eeaff28bf53b5d15e88bb7ec9b77698138fb15915f1ae/buildai_cli-0.2.0-py3-none-any.whl"
+  sha256 "78137b4a716cdd4bcb70337b7710ccdf9d37e8ed93f4c4f7b61e9dbfe3d5abcb"
+  version "0.2.0"
   depends_on "python@3.11"
   def install
     wheel = Dir["*.whl"].first
